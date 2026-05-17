3b2bb14c152192d9488840a68580c66793a97435
ViveBTL <vova.tikh0@gmail.com>
2026-05-18 02:55:11 +0300

    Update print.h

--- include/print.h
+++ include/print.h
@@ -1,5 +1,11 @@
+#ifndef PRINT_H
+#define PRINT_H
+
 #include <string>
+#include <iostream>
 #include <fstream>
 
-void print(const std::string& text, std::ofstream& out);
 void print(const std::string& text, std::ostream& out = std::cout);
+void print(const std::string& text, std::ofstream& out);
+
+#endif 
